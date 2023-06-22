b67c7758cc1ef2ccb5dec1153639c98dd260fa75
Husam A.K.A. Karman <husam@DESKTOP-263DOQN.(none)>
2023-06-22 15:14:12 +0300

    First commit

---
+++ number.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number: "))
+if n > 0:
+    print(f"The number {n} you have entered is Posative.")
+elif n < 0:
+    print(f"The number {n} you have entered is Nigative.")
+else:
+    print(f"The number {n} which you have entered is not a Number")
